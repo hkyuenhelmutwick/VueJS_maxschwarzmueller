bc380ff0bfe2ad69ffbea0e82495ecfac4c4f7ed
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2025-02-05 21:21:07 +0800

    03-adding-a-heal-functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      currentRound: 0
+      currentRound: 0,
     };
   },
   computed: {
@@ -19,7 +19,7 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
-    }
+    },
   },
   methods: {
     attackMonster() {
@@ -37,7 +37,17 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
-    }
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
